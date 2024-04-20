864e175cc24daaf9be4d0ce9e0aca75f65528ce1
Mohamedamineberguia <mohamedamineberguia@gmail.com>
2024-04-19 18:33:14 -0700

    configuration

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,3 +3,13 @@ package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
+
+package { 'Werkzeug':
+  ensure   => '2.1.1', # Use the correct version compatible with Flask 2.1.0
+  provider => 'pip3',
+}
+
+# Notify the user about the installation
+notify { 'Flask installation completed':
+  require => [Package['Flask'], Package['Werkzeug']],
+}
